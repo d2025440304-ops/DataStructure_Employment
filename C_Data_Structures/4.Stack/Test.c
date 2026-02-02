09998a6b9d48955d1fcccb3e0fb3c7bd119dd99c
Yu-dai <d2025440304@gmail.com>
2026-02-02 23:02:36 +0800

    测试

--- C_Data_Structures/4.Stack/Test.c
+++ C_Data_Structures/4.Stack/Test.c
@@ -1,19 +1,4 @@
 #include "Stack.h"
-// int main()
-// {
-//   ST s;
-//   STInit(&s);
-//   STPush(&s,1);
-//   STPush(&s,2);
-//   STPush(&s,3);
-//   STPush(&s,4);
-//   STPush(&s,5);
-//   printf("%d\n",STTop(&s));
-//   STPop(&s);
-//   printf("%d\n",STTop(&s));
-//   return 0;
-// }
-
 int main()
 {
   ST s;
