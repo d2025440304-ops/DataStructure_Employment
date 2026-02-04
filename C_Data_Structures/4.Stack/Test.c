15235a78c83a6aead3710539de370e067ab63477
Yu-dai <d2025440304@gmail.com>
2026-02-04 22:13:34 +0800

    测试

--- C_Data_Structures/4.Stack/Test.c
+++ C_Data_Structures/4.Stack/Test.c
@@ -1,18 +1,1 @@
 #include "Stack.h"
-int main()
-{
-  ST s;
-  STInit(&s);
-  STPush(&s,1);
-  STPush(&s,2);
-  STPush(&s,3);
-  STPush(&s,4);
-  STPush(&s,5);
-  //访问栈内的所有元素
-  while (!STEmpty(&s))
-  {
-    printf("%d ",STTop(&s));
-    STPop(&s);
-  }
-  return 0;
-}
